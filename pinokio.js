86a6bfbb4594d41fb5962f4e9e7ac3ecc4996f3c
Jerome B. <77253921+keeliman@users.noreply.github.com>
2025-01-16 23:03:56 +0100

    Update pinokio.js

--- pinokio.js
+++ pinokio.js
@@ -1,8 +1,9 @@
-{
-  "name": "TANGO",
-  "version": "1.0.0",
-  "description": "Application pour le réenactement vidéo de gestes synchronisés avec la parole.",
-  "scripts": {
-    "install": "install.json"
+module.exports = {
+  name: "TANGO",
+  version: "1.0.0",
+  description: "Application pour le réenactement vidéo de gestes synchronisés avec la parole.",
+  scripts: {
+    install: "install.json",
+    start: "start.json"
   }
-}
+};
