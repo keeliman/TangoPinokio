4318741a5230585395c5157875d2ced4ff1f42fb
Jerome B. <77253921+keeliman@users.noreply.github.com>
2025-01-18 02:41:39 +0100

    Update start.js

--- start.js
+++ start.js
@@ -1,10 +1,31 @@
-class Starter {
-  async run(req, ondata, kernel) {
-    const shell = kernel.shell;
+module.exports = {
+  daemon: true,
+  run: [
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Spécifiez le dossier de l'environnement virtuel
+        env: {},                    // Variables d'environnement personnalisées si nécessaire
+        path: "app",                // Chemin vers le répertoire de l'application
+        message: [
+          "python app.py",          // Commande pour démarrer l'application TANGO
+        ],
+        on: [{
+          // Expression régulière pour détecter l'URL de démarrage de l'application
+          event: "/http:\\/\\/\\S+/",
 
-    // Activer l'environnement 'tango' et lancer l'application
-    await shell.run('conda activate tango && python app.py', { cwd: 'TANGO' });
-  }
-}
-
-module.exports = Starter;
+          // Passe à l'étape suivante tout en maintenant le shell actif
+          done: true
+        }]
+      }
+    },
+    {
+      // Définit la variable locale 'url' pour utilisation dans pinokio.js
+      method: "local.set",
+      params: {
+        // input.event contient l'objet de correspondance de l'expression régulière de l'étape précédente
+        url: "{{input.event[0]}}"
+      }
+    }
+  ]
+};
