69c4a49888a4c82003daee9a3c1245894edc0100
Jerome B. <77253921+keeliman@users.noreply.github.com>
2025-01-18 02:35:48 +0100

    Update install.js

--- install.js
+++ install.js
@@ -1,20 +1,42 @@
-class Installer {
-  async run(req, ondata, kernel) {
-    const shell = kernel.shell;
-
-    // Cloner le dépôt principal TANGO
-    await shell.run('git clone https://github.com/CyberAgentAILab/TANGO.git');
-
-    // Cloner les sous-dépôts requis dans le répertoire TANGO
-    await shell.run('git clone https://github.com/justinjohn0306/Wav2Lip.git', { cwd: 'TANGO' });
-    await shell.run('git clone https://github.com/dajes/frame-interpolation-pytorch.git', { cwd: 'TANGO' });
-
-    // Créer un environnement Conda nommé 'tango' avec Python 3.9.20
-    await shell.run('conda create -n tango python=3.9.20 -y');
-
-    // Activer l'environnement 'tango' et installer les dépendances
-    await shell.run('conda activate tango && pip install -r pre-requirements.txt && pip install -r requirements.txt', { cwd: 'TANGO' });
-  }
-}
-
-module.exports = Installer;
+module.exports = {
+  run: [
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/CyberAgentAILab/TANGO.git app",
+        ]
+      }
+    },
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",
+          path: "app",
+        }
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",
+        path: "app",
+        message: [
+          "git clone https://github.com/justinjohn0306/Wav2Lip.git",
+          "git clone https://github.com/dajes/frame-interpolation-pytorch.git",
+          "pip install -r pre-requirements.txt",
+          "pip install -r requirements.txt"
+        ]
+      }
+    },
+    {
+      when: "{{platform !== 'darwin'}}",
+      method: "fs.link",
+      params: {
+        venv: "app/env"
+      }
+    }
+  ]
+};
